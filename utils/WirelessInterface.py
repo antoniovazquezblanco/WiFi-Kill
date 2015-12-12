124b6dbd131cb732555d83ee2ebadb2a4445e1f8
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2015-12-12 01:09:11 +0100

    Improve wireless interface implementation.

--- utils/WirelessInterface.py
+++ utils/WirelessInterface.py
@@ -11,52 +11,74 @@ DEVNULL = open(os.devnull, 'w')
 
 
 class WirelessInterface:
-    def __init__(self, name, mac, mode):
-        self.name = name
-        self.mac = mac
-        self.mode = mode
+	def __init__(self, name):
+		self.name = name
+		self.mac = '<unknown>'
+		self.mode = '<unknown>'
 
-    def get_as_tuple(self):
-        return self.name, self.mac, self.mode
+	def set_mac(self, mac):
+		self.mac = mac
 
-    def mac_randomize(self):
-        random.seed()
-        new_mac = self.mac
-        while new_mac == self.mac:
-            new_mac = ''
-            for i in range(0, 12):
-                if i % 2 == 0 and i != 0:
-                    new_mac += ':'
-                new_mac += '0123456789ABCDEF'[random.randint(0, 15)]
-        call(['ifconfig', self.name, 'down'])
-        call(['ifconfig', self.name, 'hw', 'ether', new_mac])
-        call(['ifconfig', self.name, 'up'])
-        print("[!] WirelessInterface.mac_randomize(): TODO: Implement checking!")
+	def set_mode(self, mode):
+		self.mode = mode
 
-    @staticmethod
-    def get_interfaces():
-	# TODO: Pipe stderr to /dev/null in Popen...
-        process = Popen(['iwconfig'], stdout=PIPE, stderr=DEVNULL)
-        interface = ''
-        mac = ''
-        interface_list = []
-        for line in process.communicate()[0].split('\n'):
-            if len(line) == 0:
-                continue
-            if ord(line[0]) != 32:                  # Doesn't start with space
-                interface = line[:line.find(' ')]   # Is the interface
-                if_process = Popen(['ifconfig', interface], stdout=PIPE)
-                if_process.wait()
-                first_line = if_process.communicate()[0].split('\n')[0]
-                for word in first_line.split(' '):
-                    if word != '':
-                        mac = word
-                    if mac.find('-') != -1:
-                        mac = mac.replace('-', ':')
-                    if len(mac) > 17:
-                        mac = mac[0:17]
-            if line.find('Mode:Monitor') != -1:
-                interface_list.append(WirelessInterface(interface, mac, 'monitor'))
-            else:
-                interface_list.append(WirelessInterface(interface, mac, 'managed'))
-        return interface_list
+	def get_as_tuple(self):
+		return self.name, self.mac, self.mode
+
+	def mac_randomize(self):
+		random.seed()
+		new_mac = self.mac
+		while new_mac == self.mac:
+			new_mac = ''
+			for i in range(0, 12):
+				if i % 2 == 0 and i != 0:
+					new_mac += ':'
+				new_mac += '0123456789ABCDEF'[random.randint(0, 15)]
+		call(['ifconfig', self.name, 'down'])
+		call(['ifconfig', self.name, 'hw', 'ether', new_mac])
+		call(['ifconfig', self.name, 'up'])
+		print("[!] WirelessInterface.mac_randomize(): TODO: Implement checking!")
+
+	@staticmethod
+	def get_interfaces():
+		# Find the interface list....
+		process = Popen(['iwconfig'], stdout=PIPE, stderr=DEVNULL)
+		process.wait()
+		interfaces = []
+		for line in process.communicate()[0].split('\n'):
+			if len(line) == 0:
+				continue
+			if ord(line[0]) != 32:				# Doesn't start with space
+				interfaces.append(WirelessInterface(line[:line.find(' ')]))	# Is the interface
+		# Get the mac address of every device...
+		for i in interfaces:
+			mac = ''
+			process = Popen(['ifconfig', i.name], stdout=PIPE, stderr=DEVNULL)
+			process.wait()
+			output = process.communicate()[0]
+			if output.find('HWaddr') != -1:		# Old ifconfig...
+				line = output.split('\n')[0]
+				for word in line.split(' '):
+					if word != '':
+						mac = word
+					if mac.find('-') != -1:
+						mac = mac.replace('-', ':')
+					if len(mac) > 17:
+						mac = mac[0:17]
+			else:					# New ifconfig...
+				line = output.split('\n')[3]
+				for word in line.split(' '):
+					if word != '' and word.find(':') != -1:
+						mac = word.upper()
+						break
+			i.set_mac(mac)
+		# Get interface mode...
+		for i in interfaces:
+			process = Popen(['iwconfig', i.name], stdout=PIPE, stderr=DEVNULL)
+			process.wait()
+			if process.communicate()[0].find('Mode:Monitor') != -1:
+				i.set_mode('Monitor')
+			else:
+				i.set_mode('Managed')
+		return interfaces
+
