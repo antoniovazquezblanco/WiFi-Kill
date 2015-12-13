479fa8a864ee72b45af739e1c937ad832823b10a
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2015-12-13 01:03:57 +0100

    Add some buttons to the interface list and implement MAC randomizing.

--- WiFiKill.py
+++ WiFiKill.py
@@ -9,46 +9,63 @@ from utils import Sniffer
 
 
 class WiFiKill:
-    def __init__(self):
-        self.builder = Gtk.Builder()
-        self.builder.add_from_file("WiFiKill.glade")
-        self.builder.connect_signals(self)
-        self.window = self.builder.get_object("window_main")
-        self.log_text = self.builder.get_object("window_main_notebook_log_text")
-        self.log_text_buff = self.log_text.get_buffer()
-        self.interface_liststore = self.builder.get_object("window_main_liststore_interfaces")
-        self.sniffer_thread = Sniffer()
+	def __init__(self):
+		self.builder = Gtk.Builder()
+		self.builder.add_from_file("WiFiKill.glade")
+		self.builder.connect_signals(self)
+		self.window = self.builder.get_object("window_main")
+		self.log_text = self.builder.get_object("window_main_notebook_log_text")
+		self.log_text_buff = self.log_text.get_buffer()
+		self.interface_treeview = self.builder.get_object("window_main_notebook_interfaces_treeview")
+		self.interface_liststore = self.builder.get_object("window_main_liststore_interfaces")
+		self.sniffer_thread = Sniffer()
 
-    def loop(self):
-        self.window.show_all()
-        Gtk.main()
+	def loop(self):
+		self.window.show_all()
+		Gtk.main()
 
-    def interfaces_clear(self):
-        self.interface_liststore.clear()
+	def interfaces_clear(self):
+		self.interface_liststore.clear()
 
-    def interface_append(self, interface):
-        self.interface_liststore.append(interface)
+	def interface_append(self, interface):
+		self.interface_liststore.append(interface)
 
-    def log_append(self, text):
-        end_iter = self.log_text_buff.get_end_iter()
-        self.log_text_buff.insert(end_iter, text)
+	def log_append(self, text):
+		end_iter = self.log_text_buff.get_end_iter()
+		self.log_text_buff.insert(end_iter, text)
 
-    def on_destroy(self, *args):
-        self.log_append("[+] Stopping sniffer...\n")
-        self.sniffer_thread.stop()
-        Gtk.main_quit(*args)
+	def on_destroy(self, *args):
+		self.log_append("[+] Stopping sniffer...\n")
+		self.sniffer_thread.stop()
+		Gtk.main_quit(*args)
 
-    def window_main_toolbar_interfaces_update_clicked(self, button):
-        self.log_append("[+] Updating interfaces...\n")
-        interfaces = WirelessInterface.get_interfaces()
-        self.interfaces_clear()
-        for i in interfaces:
-            self.interface_append(i.get_as_tuple())
+	def window_main_toolbar_interfaces_update_clicked(self, button):
+		self.log_append("[+] Updating interfaces...\n")
+		interfaces = WirelessInterface.get_interfaces()
+		self.interfaces_clear()
+		for i in interfaces:
+			self.interface_append(i.get_as_tuple())
 
-    def window_main_toolbar_scan_clicked(self, button):
-        self.log_append("[+] Starting sniffer...\n")
-        self.sniffer_thread.start()
+	def window_main_toolbar_scan_clicked(self, button):
+		self.log_append("[+] Starting sniffer...\n")
+		self.sniffer_thread.start()
+
+	def window_main_notebook_interfaces_randomize_mac_clicked(self, button):
+		self.log_append("[+] TODO: Implement MAC randomization...\n")
+		# Get the selected rows
+		model, paths = self.interface_treeview.get_selection().get_selected_rows()
+		for path in paths:
+			WirelessInterface.get_from_name(model.get(model.get_iter(path), 0)[0]).mac_randomize()
+		self.window_main_toolbar_interfaces_update_clicked(button)
+		print("[!] TODO: Show dialog if no interface is selected...")
+
+	def window_main_notebook_interfaces_monitor_start_clicked(self, button):
+		print("[!] TODO: Implement monitor start...\n")
+
+	def window_main_notebook_interfaces_monitor_stop_clicked(self, button):
+		print("[+] TODO: Implement monitor stop...\n")
+
 
 if __name__ == "__main__":
-    win = WiFiKill()
-    win.loop()
+	win = WiFiKill()
+	win.loop()
--- utils/WirelessInterface.py
+++ utils/WirelessInterface.py
@@ -11,10 +11,13 @@ DEVNULL = open(os.devnull, 'w')
 
 
 class WirelessInterface:
-	def __init__(self, name):
-		self.name = name
+	def __init__(self):
+		self.name = '<unknown>'
 		self.mac = '<unknown>'
 		self.mode = '<unknown>'
+
+	def set_name(self, name):
+		self.name = name
 
 	def set_mac(self, mac):
 		self.mac = mac
@@ -27,17 +30,49 @@ class WirelessInterface:
 
 	def mac_randomize(self):
 		random.seed()
-		new_mac = self.mac
-		while new_mac == self.mac:
-			new_mac = ''
-			for i in range(0, 12):
-				if i % 2 == 0 and i != 0:
-					new_mac += ':'
-				new_mac += '0123456789ABCDEF'[random.randint(0, 15)]
+		new_mac = self.mac[:8]
+		for i in xrange(0, 6):
+			if i % 2 == 0:
+				new_mac += ':'
+			new_mac += '0123456789ABCDEF'[random.randint(0, 15)]
 		call(['ifconfig', self.name, 'down'])
 		call(['ifconfig', self.name, 'hw', 'ether', new_mac])
 		call(['ifconfig', self.name, 'up'])
-		print("[!] WirelessInterface.mac_randomize(): TODO: Implement checking!")
+
+	@staticmethod
+	def get_from_name(name):
+		interface = WirelessInterface()
+		interface.set_name(name)
+		# Get the mac address...
+		mac = ''
+		process = Popen(['ifconfig', name], stdout=PIPE, stderr=DEVNULL)
+		process.wait()
+		output = process.communicate()[0]
+		if output.find('HWaddr') != -1:		# Old ifconfig...
+			line = output.split('\n')[0]
+			for word in line.split(' '):
+				if word != '':
+					mac = word
+				if mac.find('-') != -1:
+					mac = mac.replace('-', ':')
+				if len(mac) > 17:
+					mac = mac[0:17]
+		else:					# New ifconfig...
+			for line in output.split('\n'):
+				if line.find('ether') != -1:
+					for word in line.split(' '):
+						if word != '' and word.find(':') != -1:
+							mac = word.upper()
+							break
+		interface.set_mac(mac)
+		# Get interface mode...
+		process = Popen(['iwconfig', name], stdout=PIPE, stderr=DEVNULL)
+		process.wait()
+		if process.communicate()[0].find('Mode:Monitor') != -1:
+			interface.set_mode('Monitor')
+		else:
+			interface.set_mode('Managed')
+		return interface
 
 	@staticmethod
 	def get_interfaces():
@@ -48,37 +83,7 @@ class WirelessInterface:
 		for line in process.communicate()[0].split('\n'):
 			if len(line) == 0:
 				continue
-			if ord(line[0]) != 32:				# Doesn't start with space
-				interfaces.append(WirelessInterface(line[:line.find(' ')]))	# Is the interface
-		# Get the mac address of every device...
-		for i in interfaces:
-			mac = ''
-			process = Popen(['ifconfig', i.name], stdout=PIPE, stderr=DEVNULL)
-			process.wait()
-			output = process.communicate()[0]
-			if output.find('HWaddr') != -1:		# Old ifconfig...
-				line = output.split('\n')[0]
-				for word in line.split(' '):
-					if word != '':
-						mac = word
-					if mac.find('-') != -1:
-						mac = mac.replace('-', ':')
-					if len(mac) > 17:
-						mac = mac[0:17]
-			else:					# New ifconfig...
-				line = output.split('\n')[3]
-				for word in line.split(' '):
-					if word != '' and word.find(':') != -1:
-						mac = word.upper()
-						break
-			i.set_mac(mac)
-		# Get interface mode...
-		for i in interfaces:
-			process = Popen(['iwconfig', i.name], stdout=PIPE, stderr=DEVNULL)
-			process.wait()
-			if process.communicate()[0].find('Mode:Monitor') != -1:
-				i.set_mode('Monitor')
-			else:
-				i.set_mode('Managed')
+			if ord(line[0]) != 32:									# Doesn't start with space
+				interfaces.append(WirelessInterface.get_from_name(line[:line.find(' ')]))	# Is the interface
 		return interfaces
 
