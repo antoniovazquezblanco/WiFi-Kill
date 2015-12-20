57091a965f6a31e471750623e4fb4eaeb2be8c91
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2015-12-20 16:07:57 +0100

    Lay future system checks work.

--- utils/SystemChecks.py
+++ utils/SystemChecks.py
@@ -9,6 +9,23 @@ class SystemChecks:
 
 	@staticmethod
 	def post_test():
-		print("[D] SystemChecks.post_test(): TODO: Implement!")
-		# TODO: Are we root?
-		# TODO: Is iwconfig present?
+		SystemChecks.__check_root()
+		SystemChecks.__check_modules()
+		SystemChecks.__check_binaries()
+		
+
+	@staticmethod
+	def __check_root():		
+		# TODO: Are we root? -> If not show a dialog and raise an exception...
+		print("[D] SystemChecks.__check_root(): TODO: Implement!")
+
+	@staticmethod
+	def __check_modules():
+		# TODO: Is scapy installed?
+		print("[D] SystemChecks.__check_modules(): TODO: Implement!")
+
+	@staticmethod
+	def __check_binaries():
+		print("[D] SystemChecks.__check_binaries(): TODO: Implement!")
+		# TODO: Is iwconfig present? -> sudo apt-get install wireless-tools
+		# TODO: WTF? iwconfig in debian is in /sbin/iwconfig and not in path...
