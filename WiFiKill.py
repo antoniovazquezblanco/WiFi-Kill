bb6660507a1a4e246c527654e1a892ee2a3b0f7a
Miguel <masanchez5000@gmail.com>
2016-01-04 16:47:16 +0100

    Added process checking

--- WiFiKill.py
+++ WiFiKill.py
@@ -7,6 +7,7 @@ from utils.SystemChecks import SystemChecks
 if __name__ == "__main__":
 	# Check if the most basic stuff in order to show a window is present...
 	SystemChecks.pre_test()
+	SystemChecks.post_test()
 
 	# Now that we know that can show a window perform the rest of the checks...
 	from WindowSplash import WindowSplash
