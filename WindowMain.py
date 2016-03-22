e0c6ecf34dd35ed8637088b0518ecbd9ea30ee4e
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2016-03-22 12:22:56 +0100

    Delete some debug code.

--- WindowMain.py
+++ WindowMain.py
@@ -37,7 +37,6 @@ class WindowMain:
 		self.log_text_buff.insert(end_iter, text)
 
 	def on_destroy(self, *args):
-		self.log_append("[+] Stopping sniffer...\n")
 		if self.update_callback_id != None:
 			GObject.source_remove(self.update_callback_id)
 		self.sniffer_thread.stop()
@@ -54,6 +53,5 @@ class WindowMain:
 		return True
 
 	def window_main_toolbar_scan_clicked(self, button):
-		self.log_append("[+] Starting sniffer...\n")
 		self.update_callback_id = GObject.timeout_add(1000, self.window_main_networks_update_callback)
 		self.sniffer_thread.start()
